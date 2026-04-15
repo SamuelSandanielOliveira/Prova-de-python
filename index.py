a5b6f732752ac696b8815cde75f097a320312755
SamuelSandanielOliveira <samuel.sandaniel.oliveira@escola.pr.gov.br>
2026-04-15 08:53:55 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+age = int(input(" digite sua idade: "))
+
+if age >= 18:
+    print("Você tem 18 anos ou mais. Você já pode ser preso, otario.")
+else:
+    print("Você tem menos de 18 anos. Você ainda não pode ser preso, sortudo.")
